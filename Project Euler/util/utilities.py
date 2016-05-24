ef5c8b5b52458fd9f6e76c8746301c7e1ef3d5ba
Walter Sagehorn <w.sagehorn@gmail.com>
2016-05-23 21:21:15 -0500

    restructured and added utils

---
+++ Project Euler/util/utilities.py
@@ -0,0 +1,29 @@
+# Utility functions for Project Euler
+# - - -
+# Walter Sagehorn
+
+import math, time
+
+## helper method for n_primes
+def is_prime(num, l):
+    for i in l:
+        if (num % i == 0):
+            return False
+        if (i > math.sqrt(num)):
+            return True
+    return True
+
+
+## calculates n prime numbers
+def n_primes(n):
+    l = [2,3,5,7]
+    i = 11
+    if n < 5:
+        return l[:n]
+    while (len(l) < n):
+        if is_prime(i, l):
+            l.append(i)
+        i += 2
+    return l
+
+# TODO: better prime algorithm
